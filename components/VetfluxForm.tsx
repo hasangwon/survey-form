a4f444909bfb84323f3d8134118d5aa15250202b
hasangwon <305243@naver.com>
2022-09-23 23:42:34 +0900

    Add : Form 생성

---
+++ components/VetfluxForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VetfluxForm = () => {
+  return (
+    <div className='border border-black w-4 h-4'>
+      <button>+++</button>
+    </div>
+  );
+};
+
+export default VetfluxForm;
